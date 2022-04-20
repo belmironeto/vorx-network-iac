ef701b526186ef1e30d135db2d53906e59f602cb
Belmiro Christo Neto <belmiro.neto@live.com>
2022-04-20 20:39:57 -0300

    Ajustando para deploy através de módulo

--- main.tf
+++ main.tf
@@ -1,91 +1,25 @@
-# Criação da VPC
-resource "aws_vpc" "vorx_vpc" {
-    cidr_block = "10.0.0.0/16"
-    enable_dns_hostnames = true
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-    tags = {
-        Name = "vorx_vpc"
-    }
+  name = "vorx-prod-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = false
+  one_nat_gateway_per_az = false
+
+  tags = {
+    Terraform = "true"
+    Environment = "Production"
+    CC = "100100042"
+    Team = "DevOps"
+  }
 }
 
-#Criação das Subnets Públicas
-resource "aws_subnet" "vorx_subnet_public1" {
-    vpc_id = aws_vpc.vorx_vpc.id
-    cidr_block = "10.0.1.0/24"
-    availability_zone = "us-east-1a"
-
-    tags = {
-        Name = "vorx_subnet_public_1"
-    }
+output "vpc_vorx_prod_id" {
+  value = module.vpc.vpc_id
 }
-
-resource "aws_subnet" "vorx_subnet_public2" {
-    vpc_id = aws_vpc.vorx_vpc.id
-    cidr_block = "10.0.3.0/24"
-    availability_zone = "us-east-1b"
-
-    tags = {
-        Name = "vorx_subnet_public_2"
-    }
-}
-
-#Criação das Subnets Privadas
-resource "aws_subnet" "vorx_subnet_private1" {
-    vpc_id = aws_vpc.vorx_vpc.id
-    cidr_block = "10.0.2.0/24"
-    availability_zone = "us-east-1a"
-
-    tags = {
-        Name = "vorx_subnet_private_1"
-    }
-}
-
-resource "aws_subnet" "vorx_subnet_private2" {
-    vpc_id = aws_vpc.vorx_vpc.id
-    cidr_block = "10.0.4.0/24"
-    availability_zone = "us-east-1b"
-    tags = {
-        Name = "vorx_subnet_private_2"
-    }
-}
-
-#Criação do Internet Gateway
-resource "aws_internet_gateway" "vorx_igw" {
-    vpc_id = aws_vpc.vorx_vpc.id
-    
-    tags = {
-        Name = "vorx_igw"
-    }
-}
-
-#Tabela de Roteamento da Rede Pública
-resource "aws_route_table" "vorx_routeTable_pub" {
-    vpc_id = aws_vpc.vorx_vpc.id
-
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.vorx_igw.id
-    }
-    
-    tags = {
-        Name = "vorx_routeTable_public"
-    }
-}
-
-resource "aws_route" "vorx_routeToInternet" {
-  route_table_id = aws_route_table.vorx_routeTable_pub.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.vorx_igw.id
-}
-
-#Associando Subnet pública com a Tabela de Roteamento
-resource "aws_route_table_association" "vorx_rt_association_pub1" {
-    subnet_id = aws_subnet.vorx_subnet_public1.id
-    route_table_id = aws_route_table.vorx_routeTable_pub.id
-}
-
-resource "aws_route_table_association" "vorx_rt_association_pub2" {
-    subnet_id = aws_subnet.vorx_subnet_public2.id
-    route_table_id = aws_route_table.vorx_routeTable_pub.id
-}
-
